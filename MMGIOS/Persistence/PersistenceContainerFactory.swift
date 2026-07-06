8889210f718f405dea702c29a530905e5beb9520
infomindsetmediagroup-cloud <info.mindsetmediagroup@gmail.com>
2026-07-06 08:42:32 -0700

    Add SwiftData container factory

---
+++ MMGIOS/Persistence/PersistenceContainerFactory.swift
@@ -0,0 +1,25 @@
+import Foundation
+import SwiftData
+
+enum PersistenceContainerFactory {
+    static let schema = Schema([
+        PersistedProjectRecord.self,
+        PersistedCustomerRequestRecord.self,
+        PersistedPublishingAssetRecord.self,
+        PersistedReleasePackageRecord.self,
+        PersistedCampaignRecord.self
+    ])
+
+    @MainActor
+    static func makeContainer(inMemory: Bool = false) throws -> ModelContainer {
+        let configuration = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: inMemory
+        )
+
+        return try ModelContainer(
+            for: schema,
+            configurations: [configuration]
+        )
+    }
+}
