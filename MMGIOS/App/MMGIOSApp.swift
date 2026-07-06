08a147a690c6845014602ee675cdd47f73c69a29
infomindsetmediagroup-cloud <info.mindsetmediagroup@gmail.com>
2026-07-06 08:45:11 -0700

    Register complete SwiftData schema at launch

--- MMGIOS/App/MMGIOSApp.swift
+++ MMGIOS/App/MMGIOSApp.swift
@@ -3,10 +3,20 @@ import SwiftUI
 
 @main
 struct MMGIOSApp: App {
+    private let modelContainer: ModelContainer
+
+    init() {
+        do {
+            modelContainer = try PersistenceContainerFactory.makeContainer()
+        } catch {
+            fatalError("Failed to initialize SwiftData model container: \(error.localizedDescription)")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             AuthGateView()
         }
-        .modelContainer(for: PersistedProjectRecord.self)
+        .modelContainer(modelContainer)
     }
 }
