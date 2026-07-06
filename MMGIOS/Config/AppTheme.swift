fa8fbbb1109ac0e345877e59c40612375b4bbf7c
infomindsetmediagroup-cloud <info.mindsetmediagroup@gmail.com>
2026-07-06 07:04:58 -0700

    Add MMG app theme

---
+++ MMGIOS/Config/AppTheme.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+extension Color {
+    static let mmgBlue = Color(red: 0.0, green: 0.48, blue: 1.0)
+    static let mmgDeepBlue = Color(red: 0.0, green: 0.12, blue: 0.38)
+    static let mmgInk = Color(red: 0.05, green: 0.07, blue: 0.12)
+    static let mmgSurface = Color(red: 0.96, green: 0.98, blue: 1.0)
+}
+
+struct AppTheme {
+    static let appName = "Kairos"
+    static let companyName = "Mindset Media Group™"
+    static let minimumIOSVersion = "17.0"
+}
