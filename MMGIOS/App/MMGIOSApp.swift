71be04ec2bff4cfc36b76ce79fbfc3981ac271d2
infomindsetmediagroup-cloud <info.mindsetmediagroup@gmail.com>
2026-07-07 16:07:10 -0700

    Simplify app entry for standalone build

--- MMGIOS/App/MMGIOSApp.swift
+++ MMGIOS/App/MMGIOSApp.swift
@@ -1,30 +1,10 @@
-import SwiftData
 import SwiftUI
 
 @main
 struct MMGIOSApp: App {
-    private let modelContainer: ModelContainer
-    @Environment(\.scenePhase) private var scenePhase
-    @State private var kairosRuntime = LocalKairosRuntimeStore.restore()
-
-    init() {
-        do {
-            modelContainer = try PersistenceContainerFactory.makeContainer()
-        } catch {
-            preconditionFailure("SwiftData model container initialization failed")
-        }
-    }
-
     var body: some Scene {
         WindowGroup {
-            AuthGateView()
-                .environment(kairosRuntime)
-                .onChange(of: scenePhase) { _, newPhase in
-                    if newPhase != .active {
-                        LocalKairosRuntimeStore.save(kairosRuntime)
-                    }
-                }
+            AppRootView()
         }
-        .modelContainer(modelContainer)
     }
 }
