fe41c9814b02934da28c144f6d720234fb0df173
infomindsetmediagroup-cloud <info.mindsetmediagroup@gmail.com>
2026-07-08 20:41:47 -0700

    feat(asset): register production asset model [skip ci]

--- MMGIOS/App/MMGIOSApp.swift
+++ MMGIOS/App/MMGIOSApp.swift
@@ -14,6 +14,7 @@ struct MMGIOSApp: App {
             TaskDependencyRecord.self,
             ProductionQueueRecord.self,
             DesignStudioProjectRecord.self,
+            ProductionAssetRecord.self,
             KnowledgeVaultRecord.self
         ])
     }
