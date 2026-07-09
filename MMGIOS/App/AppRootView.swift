8a15ae747d643d299ca66e899280515b1cfcaa27
infomindsetmediagroup-cloud <info.mindsetmediagroup@gmail.com>
2026-07-08 18:38:19 -0700

    feat(runtime): register workflow preview models [skip ci]

--- MMGIOS/App/AppRootView.swift
+++ MMGIOS/App/AppRootView.swift
@@ -56,6 +56,8 @@ struct AppRootView: View {
     AppRootView()
         .modelContainer(for: [
             PersistedCustomerRequestRecord.self,
-            PersistedValueDiscoveryProfile.self
+            PersistedValueDiscoveryProfile.self,
+            WorkflowRecord.self,
+            WorkflowTransitionRecord.self
         ], inMemory: true)
 }
