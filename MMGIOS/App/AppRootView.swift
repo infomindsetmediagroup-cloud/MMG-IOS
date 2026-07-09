10264119607511ecade5f53a37014388e86cf597
infomindsetmediagroup-cloud <info.mindsetmediagroup@gmail.com>
2026-07-08 23:33:00 -0700

    fix: align root preview SwiftData schema [skip ci]

--- MMGIOS/App/AppRootView.swift
+++ MMGIOS/App/AppRootView.swift
@@ -56,6 +56,9 @@ struct AppRootView: View {
     AppRootView()
         .modelContainer(for: [
             PersistedCustomerRequestRecord.self,
-            PersistedValueDiscoveryProfile.self
+            PersistedValueDiscoveryProfile.self,
+            CustomerReleaseRecord.self,
+            DeliverableRecord.self,
+            ProductionAssetRecord.self
         ], inMemory: true)
 }
