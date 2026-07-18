f550270903c22f503f9be62c2d3e0f9c7004403a
infomindsetmediagroup-cloud <info.mindsetmediagroup@gmail.com>
2026-07-17 22:45:43 -0700

    Include ecosystem navigation styles in homepage bundle

--- cloudflare/mmg-ios/src/kairos-canonical-homepage-source-20260718.js
+++ cloudflare/mmg-ios/src/kairos-canonical-homepage-source-20260718.js
@@ -17,6 +17,7 @@ import { SECTION_SOURCE_A } from "./kairos-canonical-homepage-source-section-a-2
 import { SECTION_SOURCE_B } from "./kairos-canonical-homepage-source-section-b-20260718.js";
 import { CSS_SOURCE_A } from "./kairos-canonical-homepage-source-css-a-20260718.js";
 import { CSS_SOURCE_B } from "./kairos-canonical-homepage-source-css-b-20260718.js";
+import { CSS_SOURCE_C } from "./kairos-canonical-homepage-source-css-c-20260718.js";
 
 export const SECTION_SOURCE = SECTION_SOURCE_A + SECTION_SOURCE_B;
-export const CSS_SOURCE = CSS_SOURCE_A + CSS_SOURCE_B;
+export const CSS_SOURCE = CSS_SOURCE_A + CSS_SOURCE_B + CSS_SOURCE_C;
